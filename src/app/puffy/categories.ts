c781d764ea611a29c75ad1084b6140259ead8128
amitharigupta <amitgt9967@gmail.com>
2026-04-11 21:13:01 +0530

    added slug for puffy

---
+++ src/app/puffy/categories.ts
@@ -0,0 +1,11 @@
+export const CATEGORIES: any = [
+    { label: 'Puffy 1', video: '/videos/puffy/1.mp4', href: 'puffy/puffy-1' },
+    { label: 'Puffy 2', video: '/videos/puffy/2.mp4', href: 'puffy/puffy-2' },
+    { label: 'Puffy 3', video: '/videos/puffy/3.mp4', href: 'puffy/puffy-3' },
+    { label: 'Puffy 4', video: '/videos/puffy/4.mp4', href: 'puffy/puffy-4' },
+    { label: 'Puffy 5', video: '/videos/puffy/5.mp4', href: 'puffy/puffy-5' },
+    { label: 'Puffy 6', video: '/videos/puffy/6.mp4', href: 'puffy/puffy-6' },
+    { label: 'Puffy 7', video: '/videos/puffy/7.mp4', href: 'puffy/puffy-7' },
+    { label: 'Puffy 8', video: '/videos/puffy/8.mp4', href: 'puffy/puffy-8' },
+    { label: 'Puffy 9', video: '/videos/puffy/9.mp4', href: 'puffy/puffy-9' },
+];
